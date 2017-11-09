1c4a5990c7febd794140fed30f54d8168df1f36b
TraceyKong <zixuan.kong70@myhunter.cuny.edu>
2017-11-09 11:07:09 -0500

    11/9

--- 10/wc.py
+++ 10/wc.py
@@ -1,3 +1,5 @@
+import random
+
 def rp(w):
     result = [l for l in w if l.isalpha()]
     return ''.join(result)
@@ -9,21 +11,24 @@ def bwcd(wordlist):
         d[w] = wordlist.count(w)
     return d
 
-def hbwcd(f):
-    f = open(f).read()
-    l = [rp(w) for w in f.lower().split()]
-    return l
-
 def bwcff(f):
-    return bwcd(hbwcd(f))
-
-def tbo(s,f):
-    tf = hbwcd(f)
-    st = [rp(w) for w in s.lower().split()]
+    f = open(f).read()
+    tf = [rp(w) for w in f.lower().split()]
     l = {}
-    for w in st:
-        l[w] = [tf[i+1] for i in range(len(tf)) if tf[i]==w]
+    for i in range(len(tf)-1):
+        l.setdefault(tf[i],[])
+        l[tf[i]].append(tf[i+1])
     return l
 
-d = bwcff("hamlet.txt")
-t = tbo('to be or not to be',"hamlet.txt")
+def generate_text(d,start_word,length=50):
+    wordlist=[]
+    next = start_word
+    for i in range(length):
+        wordlist.append(next)
+        next = random.choice(d[next])
+    return ' '.join(wordlist)
+
+hamlet = bwcff("hamlet.txt")
+psalms = bwcff("psalms.txt")
+sonnets = bwcff("sonnets.txt")
+g = generate_text(sonnets,'to')
