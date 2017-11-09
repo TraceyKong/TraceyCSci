1fd261b94984ae90b144d5fa286bfde720fd5c5f
TraceyKong <31457793+TraceyKong@users.noreply.github.com>
2017-11-09 01:15:28 -0500

    Update wc.py
    11/8

--- 10/wc.py
+++ 10/wc.py
@@ -8,10 +8,21 @@ def bwcd(wordlist):
         d[w] = wordlist.count(w)
     return d
 
-def bwcff(f):
+def hbwcd(f):
     f = open(f).read()
     l = [rp(w) for w in f.lower().split()]
-    return bwcd(l)
+    return l
 
+def bwcff(f):
+    return bwcd(hbwcd(f))
+
+def tbo(s,f):
+    tf = hbwcd(f)
+    st = [rp(w) for w in s.lower().split()]
+    l = {}
+    for w in st:
+        l[w] = [tf[i+1] for i in range(len(tf)) if tf[i]==w]
+    return l
 
 d = bwcff("hamlet.txt")
+t = tbo('to be or not to be',"hamlet.txt")
