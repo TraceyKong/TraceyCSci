6b7d8c1851e9bcea529648dae87ed5879df8cef0
TraceyKong <zixuan.kong70@myhunter.cuny.edu>
2017-11-15 21:37:45 -0500

    11/15

--- 12/csv_hw.py
+++ 12/csv_hw.py
@@ -1,21 +1,44 @@
 import csv
 
-f = open("sample-texts.csv")
-sampleText = [line for line in csv.reader(f)]
-f.close()
-f = open("offenders-clean.csv")
-offendersClean = [line for line in csv.DictReader(f)]
-f.close()
-
 def inverted_index_reader(l):
+    f = open(l)
+    data =  [line for line in csv.reader(f)]
+    f.close()
     wordList= {}
-    for word in [w for wl in l for w in wl[1].split(' ')]:
-        wordList.setdefault(word,[line[0] for line in l if word in line[1]])
+    for word in [w for wl in data for w in wl[1].split(' ')]:
+        wordList.setdefault(word,[line[0] for line in data if word in line[1]])
     return wordList
+    
+def reader_searcher(searchWord,d):
+    if searchWord in d.keys():
+        for l in d[searchWord]:
+            print(l)
+        return
+    print('no result')
 
 def inverted_index_dict(l):
-    return l
+    file = open(l)
+    f = csv.DictReader(file)
+    headers = f.fieldnames
+    doc = [line for line in f]
+    file.close()
+    d = {}
+    for i in headers[1:]:
+        temp = {}
+        for j in doc:
+            temp.setdefault(j[i],[])
+            temp[j[i]].append(j["Execution #"])
+        d.setdefault(i,temp)
+    return d
     
+def dict_reader_searcher(attribute,searchWord,d):
+    if attribute in d.keys():
+        temp = d[attribute]
+        if searchWord in temp.keys():
+            for l in temp[searchWord]:
+                print(l)
+            return
+    print('no result')
 
-reader = inverted_index_reader(sampleText)
-dreader = inverted_index_dict(offendersClean)
+sample_text = inverted_index_reader("sample-texts.csv")
+offenders_clean = inverted_index_dict("offenders-clean.csv")
