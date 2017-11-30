fa7189ed189f95626c41d34b1bae520ca546f543
TraceyKong <zixuan.kong70@myhunter.cuny.edu>
2017-11-30 11:16:43 -0500

    c++

---
+++ notes/11-30-kong-tracey-cppintro.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int addtwo(int a, int b){
+  return a+b;
+}
+  
+int main(){
+  int i = 29;
+  int j = 30;
+  int result = addtwo(i,j);
+  std::cout << result << "\n";
+  std::cout << "hello!\n";
+}
+
